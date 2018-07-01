db6b4c8925c6edceee8393fc5c641115ab0472f8
huynvn010193 <hpn010193@gmail.com>
2018-07-01 20:54:59 +0700

    destructuring array

--- 01-JS-NangCao/js/01-Destructuring.js
+++ 01-JS-NangCao/js/01-Destructuring.js
@@ -14,3 +14,20 @@ let user = {
 // Destructuring.
 const { firstname, lastname, email, address } = user;
 const { block, number, street = "PVT" } = address
+
+// Destructuring. Array
+var names = ['Khoa','Hung','Hanh','Linh','Hanh'];
+const [first,seacond,third,...girlname] = names;
+
+// Map
+var students = new Map();
+students.set('id1',{name:'Khoa'});
+students.set('id2',{name:'Huy'});
+students.set('id3',{name:'Vũ'});
+
+for ([id,info] of students)
+{
+    const {name,age = 7} = info;
+    console.log(`${name} is ${age} years  old and has id: ${id}`);
+}
+
