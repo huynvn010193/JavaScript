8e0fb6154fcf43d72df396a953036659a2a2c7b4
huynvn010193 <hpn010193@gmail.com>
2019-03-26 19:13:50 +0700

    Them ve reduce

--- 01-JS-NangCao/js/03-Reducer.js
+++ 01-JS-NangCao/js/03-Reducer.js
@@ -5,16 +5,17 @@ for (let i = 0; i < numArr.length; i++) {
     total += numArr[i];
 }
 
-console.log(`For: ${total}`);
+// console.log(`For: ${total}`);
 
 total = 0;
 total = numArr.reduce((accmalator, currentValue) => {
-    console.log(`accmalator: ${accmalator} - currentValue: ${currentValue}`);
+    // console.log(`accmalator: ${accmalator} - currentValue: ${currentValue}`);
     return accmalator + currentValue;
 });
 
-console.log(`Reduce: ${total}`);
+// console.log(`Reduce: ${total}`);
 
+// Tương tác với array Object
 const arrObj = [
     {
         name: 'An Vu'
@@ -28,12 +29,19 @@ const arrObj = [
 ];
 
 const newObject = arrObj.reduce((accmalator, currentValue) => {
-    console.log(accmalator);
-    console.log(currentValue);
+    // console.log(accmalator);
+    // console.log(currentValue);
     return {
       ...accmalator,
       ...currentValue
     };
 });
 
-console.log(newObject);
+// console.log(newObject);
+
+// Tương tác với object
+const obj = {
+	name: 'An Vu',
+	job: 'CEO',
+	age: 30
+}
