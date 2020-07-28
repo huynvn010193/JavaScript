9780e7cdeaf05cdb5f9871ed1a5b1c63954ecd05
huynvn010193 <hpn010193@gmail.com>
2020-07-28 11:23:47 +0700

    Update code reduce

--- 01-JS-NangCao/js/03-Reducer.js
+++ 01-JS-NangCao/js/03-Reducer.js
@@ -1,47 +1,47 @@
-const numArr = [1,2,3,4];
+const numArr = [1, 2, 3, 4];
 let total = 0;
 
 for (let i = 0; i < numArr.length; i++) {
-    total += numArr[i];
+  total += numArr[i];
 }
 
 // console.log(`For: ${total}`);
 
 total = 0;
 total = numArr.reduce((accmalator, currentValue) => {
-    // console.log(`accmalator: ${accmalator} - currentValue: ${currentValue}`);
-    return accmalator + currentValue;
+  // console.log(`accmalator: ${accmalator} - currentValue: ${currentValue}`);
+  return accmalator + currentValue;
 });
 
 // console.log(`Reduce: ${total}`);
 
 // Tương tác với array Object
 const arrObj = [
-    {
-        name: 'An Vu'
-    },
-    {
-        job: 'CEO'
-    },
-    {
-        age: 30
-    }
+  {
+    name: "An Vu",
+  },
+  {
+    job: "CEO",
+  },
+  {
+    age: 30,
+  },
 ];
 
 const newObject = arrObj.reduce((accmalator, currentValue) => {
-    // console.log(accmalator);
-    // console.log(currentValue);
-    return {
-      ...accmalator,
-      ...currentValue
-    };
+  // console.log(accmalator);
+  // console.log(currentValue);
+  return {
+    ...accmalator,
+    ...currentValue,
+  };
 });
 
-// console.log(newObject);
+console.log(newObject);
 
 // Tương tác với object
 const obj = {
-	name: 'An Vu',
-	job: 'CEO',
-	age: 30
-}
+  name: "An Vu",
+  job: "CEO",
+  age: 30,
+};
