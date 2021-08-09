1b468166df404c7b23c63e2118abe419b436ae08
huynvn010193 <hpn010193@gmail.com>
2021-08-09 18:52:51 +0700

    Add closure

---
+++ 01-JS-NangCao/js/04-closure.js
@@ -0,0 +1,49 @@
+// Ứng dụng tính closure ghi log.
+
+const createLogger = (namespace) => {
+  const logger = (message) => {
+    console.log(`[${namespace}] ${message}`);
+  };
+
+  return logger;
+};
+
+// ============== APP =================
+const infoLogger = createLogger("Info");
+infoLogger("Bắt đầu gửi mail");
+infoLogger("Gửi mail lần 1");
+
+const infoLogger2 = createLogger("Error");
+infoLogger2("Email không tồn tại");
+infoLogger2("Gửi mail thất bại");
+
+// Ứng dụng lưu storage.
+
+const createStore = (key) => {
+  const store = JSON.parse(localStorage.getItem(key)) ?? {};
+  const save = () => {
+    localStorage.setItem(key, JSON.stringify(store));
+  };
+
+  const storage = {
+    get(key) {
+      return store[key];
+    },
+    set(key, value) {
+      store[key] = value;
+      save();
+    },
+    remove(key) {
+      delete store[key];
+      save();
+    },
+  };
+
+  return storage;
+};
+
+//Profile.js
+const profilteSetting = createStore("profile_setting");
+console.log(profilteSetting.get("fullName"));
+// profilteSetting.set("fullName", "Huy Nguyen");
+profilteSetting.remove("fullName", "Huy Nguyen");
