668f175801ccf21e9c6e256e1a8edf6e880507a7
huynvn010193 <hpn010193@gmail.com>
2021-08-11 15:04:45 +0700

    Tu khoa this

---
+++ 01-JS-NangCao/js/05-this.js
@@ -0,0 +1,13 @@
+const iPhone7 = {
+  // thuộc tính - property
+  name: "Iphone 7",
+  color: "pink",
+  weight: "300",
+
+  // Phương thức
+  takePhoto() {
+    console.log(this);
+  },
+};
+
+console.log(iPhone7.takePhoto());
